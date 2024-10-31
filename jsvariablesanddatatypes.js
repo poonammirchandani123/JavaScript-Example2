14c077f8f90428b4611d3cec95f155118f029bf8
Permeshwary(Poonam) Bora <147245184+poonammirchandani123@users.noreply.github.com>
2024-10-31 09:31:40 +0000

    Add files via upload

---
+++ jsvariablesanddatatypes.js
@@ -0,0 +1,21 @@
+//multiple line
+//var x;  //declaration
+//x=10;  //initialization
+
+//single statement declaration+initialization
+//var x=10;
+
+//single statement - multiple variable
+//var x=100,y=200;
+
+//document.write(x)
+//console.log(x)
+
+//**** let */
+
+let x;
+x="welcome";
+console.log(x);
+
+
+
